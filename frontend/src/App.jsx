83c835dae223f6c991c56f99383ea88a88ee6b0b
Snide34 <ghoshkhyat@gmail.com>
2026-04-06 15:10:27 +0530

    feat: setup React Router with Home and Triage routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import TriageResultsPage from './pages/TriageResultsPage';
 
 function App() {
-  return <TriageResultsPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/triage" element={<TriageResultsPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
